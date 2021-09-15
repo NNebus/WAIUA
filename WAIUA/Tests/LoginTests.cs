1069c1a7efa98d919bfb13ecb7d97aa5f6fcc8d4
Nicolai <nicolainebus@googlemail.com>
2021-09-15 13:19:37 +0200

    Added Tests for Login, Get Tokens and get player

---
+++ WAIUA/Tests/LoginTests.cs
@@ -0,0 +1,47 @@
+﻿using System.Net;
+using WAIUA.Models;
+using WAIUA.Services;
+using Xunit;
+
+namespace WAIUA.Tests
+{
+    public class LoginTests
+    {
+
+        private static Account GetAccount() {
+            Account account = new(new CookieContainer()) { Username = "", Password = "" };
+
+            if (string.IsNullOrEmpty(account.AccessToken))
+            {
+                account.GetTokens();
+            }
+                
+            return account;
+        }
+
+        [Fact]
+        public void GetJwtToken()
+        {
+            Account account = GetAccount();
+            Assert.False(string.IsNullOrEmpty(account.AccessToken));
+        }
+
+        [Fact]
+        public void GetPlayer() {
+            Account account = GetAccount();
+            ValorantApiService valorantApiService = new(account);
+            Player player = valorantApiService.GetPlayer();
+
+            Assert.False(string.IsNullOrEmpty(player.NameWithTag));
+        }
+
+        [Fact]
+        public void GetLocalRegion() {
+            Account account = GetAccount();
+            ValorantApiService valorantApiService = new(account);
+            string localRegion = valorantApiService.GetLocalRegion();
+
+            Assert.False(string.IsNullOrEmpty(localRegion));
+        }
+    }
+}
