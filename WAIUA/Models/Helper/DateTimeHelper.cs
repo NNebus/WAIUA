e8f67a0f7945eecf4401be228ecc5f0b2a12cd92
Nicolai <nicolainebus@googlemail.com>
2021-09-22 18:08:55 +0200

    Created DateTimeHelper
    Helpful for formatting and calucaltions

---
+++ WAIUA/Models/Helper/DateTimeHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WAIUA.Models.Helper
+{
+    public abstract class DateTimeHelper
+    {
+        public static DateTime GetDateTimeFromMilliseconds(ulong milliseconds) => (new DateTime(1970, 1, 1)).AddMilliseconds(milliseconds);
+        public static DateTime GetDateTimeWithAddedMilliseconds(DateTime originalDateTime, ulong millisecondsToAdd) => originalDateTime.AddMilliseconds(millisecondsToAdd);
+        public static DateTime GetDateTimeFromAddedMilliseconds(IEnumerable<ulong> milliseconds) {
+            ulong sum = 0;
+            foreach (ulong millisecondsToAdd in milliseconds) { 
+                sum += millisecondsToAdd;
+            }
+            return GetDateTimeFromMilliseconds(sum);
+        }
+    }
+}
